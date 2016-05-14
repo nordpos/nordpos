4fdce52dee6ed932bb34ce2ee1e73849813d01ce
svininykh <svininykh@gmail.com>
2016-05-14 22:28:31 +0600

    Major: Create Geo Location Map bag template.

--- src-pos/com/openbravo/pos/sales/JTicketsBag.java
+++ src-pos/com/openbravo/pos/sales/JTicketsBag.java
@@ -16,44 +16,55 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with Openbravo POS.  If not, see <http://www.gnu.org/licenses/>.
-
 package com.openbravo.pos.sales;
 
+import com.nordpos.sales.geomap.JTicketsBagLocationMap;
 import com.openbravo.pos.sales.simple.JTicketsBagSimple;
-import com.openbravo.pos.forms.*; 
+import com.openbravo.pos.forms.*;
 import javax.swing.*;
 import com.openbravo.pos.sales.restaurant.JTicketsBagRestaurantMap;
 import com.openbravo.pos.sales.shared.JTicketsBagShared;
 
 public abstract class JTicketsBag extends JPanel {
-    
-    protected AppView m_App;     
+
+    protected AppView m_App;
     protected DataLogicSales m_dlSales;
-    protected TicketsEditor m_panelticket;    
-    
-    /** Creates new form JTicketsBag */
-    public JTicketsBag(AppView oApp, TicketsEditor panelticket) {        
-        m_App = oApp;     
-        m_panelticket = panelticket;        
+    protected TicketsEditor m_panelticket;
+
+    /**
+     * Creates new form JTicketsBag
+     */
+    public JTicketsBag(AppView oApp, TicketsEditor panelticket) {
+        m_App = oApp;
+        m_panelticket = panelticket;
         m_dlSales = (DataLogicSales) m_App.getBean("com.openbravo.pos.forms.DataLogicSales");
     }
-    
+
     public abstract void activate();
+
     public abstract boolean deactivate();
+
     public abstract void deleteTicket();
-    
+
     protected abstract JComponent getBagComponent();
+
     protected abstract JComponent getNullComponent();
-    
+
     public static JTicketsBag createTicketsBag(String sName, AppView app, TicketsEditor panelticket) {
-        
-        if ("standard".equals(sName)) {
-            // return new JTicketsBagMulti(oApp, user, panelticket);
-            return new JTicketsBagShared(app, panelticket);
-        } else if ("restaurant".equals(sName)) {
-            return new JTicketsBagRestaurantMap(app, panelticket);
-        } else { // "simple"
-            return new JTicketsBagSimple(app, panelticket);
+
+        if (null != sName) {
+            switch (sName) {
+                case "standard":
+                    return new JTicketsBagShared(app, panelticket);
+                case "restaurant":
+                    return new JTicketsBagRestaurantMap(app, panelticket);
+                case "geomap":
+                    return new JTicketsBagLocationMap(app, panelticket);
+                default:
+                    break;
+            }
         }
-    }   
+        // "simple"
+        return new JTicketsBagSimple(app, panelticket);
+    }
 }
